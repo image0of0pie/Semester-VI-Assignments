19a3e6c20c2714be89002507cd354b9aa24b21ef
Sarvajit Kumar <sjsharma1415@gmail.com>
2021-03-03 18:18:09 +0530

    Completed Assignment 2

--- IT/Assignment2/server.js
+++ IT/Assignment2/server.js
@@ -1,20 +1,49 @@
 const io = require("socket.io")(3000); // starts the server at the port 3000
-const users = {}; // to store all the users list
-
+const messageStore = {};
+const authStore = {};
 io.on("connection", (socket) => {
-  socket.on("new-user", (name) => {
-    users[socket.id] = name;
-    socket.broadcast.emit("user-connected", name);
+  socket.on("new-user", ({ name, password }) => {
+    if (name in authStore) {
+      if (authStore[name] === password) {
+        io.emit("connection-status", { name, status: true });
+        socket.broadcast.emit("user-connected", name);
+        const messagesPrevious = messageStore[name];
+        console.log(messagesPrevious);
+        socket.join(name);
+        io.emit("users-list", Object.keys(authStore));
+        io.to(name).emit("prev-chat-message", messagesPrevious);
+      } else {
+        io.emit("connection-status", { name, status: false });
+      }
+    } else {
+      authStore[name] = password;
+      messageStore[name] = [];
+      io.emit("connection-status", { name: name, status: true });
+      socket.broadcast.emit("user-connected", name);
+      socket.join(name);
+      io.emit("users-list", Object.keys(authStore));
+    }
   });
-  socket.on("send-chat-message", ({ message, image, time }) => {
-    socket.broadcast.emit("chat-message", {
-      message: message,
-      image: image,
-      name: users[socket.id],
-      time: time,
-    });
+  socket.on("chat-message", ({ message, image, time, name, recipients }) => {
+    messageStore[name].push({ message, image, time, name: "You" });
+    if (name in authStore) {
+      for (idx = 0; idx < recipients.length; idx++) {
+        const receiver = recipients[idx];
+        if (receiver === name) {
+          continue;
+        }
+        messageStore[receiver].push({ message, image, time, name });
+        io.to(recipients[idx]).emit("chat-message", {
+          message,
+          image,
+          time,
+          name,
+        });
+      }
+    }
   });
   socket.on("disconnect", () => {
-    socket.broadcast.emit("user-disconnected", users[socket.id]);
+    // io.emit("user-disconnected", usersOnline[socket.id]);
+    io.emit("users-list", Object.keys(authStore));
   });
 });
--- IT/socketChat/routes/main.js
+++
@@ -1,9 +0,0 @@
-let home = require("./home"),
-    login = require('./login'),
-    room = require("./room")
-
-module.exports = {
-    "/": home,
-    "/room": room["router"], 
-    "/login" : login
-}
